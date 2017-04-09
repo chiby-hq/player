776ba6392493fd010ac75beb17a76016412b110c
Brice Copy <brice.copy@gmail.com>
2017-04-09 16:54:21 +0200

    Working REST HATEOAS repositories

--- src/main/java/com/github/chiby/player/PlayerApplication.java
+++ src/main/java/com/github/chiby/player/PlayerApplication.java
@@ -3,9 +3,12 @@ package com.github.chiby.player;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.map.repository.config.EnableMapRepositories;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableMapRepositories("com.github.chiby.player.repositories")
+@EnableMapRepositories
+@EnableSpringDataWebSupport
 public class PlayerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/github/chiby/player/model/repositories/PlaylistRepository.java
+++ src/main/java/com/github/chiby/player/model/repositories/PlaylistRepository.java
@@ -3,10 +3,13 @@ package com.github.chiby.player.model.repositories;
 
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.github.chiby.player.model.Playlist;
 
-public interface PlaylistRepository extends CrudRepository<Playlist, String>, QueryDslPredicateExecutor<Playlist>, PlaylistRepositoryCustom
+@RepositoryRestResource()
+public interface PlaylistRepository extends PagingAndSortingRepository<Playlist, String>, QueryDslPredicateExecutor<Playlist>, PlaylistRepositoryCustom
 {
 }
 
