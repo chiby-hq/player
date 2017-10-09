a0bcff0ff71fcb5d81e038072094bdc0ee2ba875
Brice Copy <brice.copy@gmail.com>
2017-10-09 11:35:35 +0200

    change to simple CRUD repository

--- modules/repositories/src/main/java/com/github/chiby/store/model/repositories/RunSessionRepository.java
+++ modules/repositories/src/main/java/com/github/chiby/store/model/repositories/RunSessionRepository.java
@@ -1,11 +1,12 @@
 package com.github.chiby.store.model.repositories;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.github.chiby.player.model.RunSession;
 
 @RepositoryRestResource()
-public interface RunSessionRepository extends PagingAndSortingRepository<RunSession, String>{
+public interface RunSessionRepository extends CrudRepository<RunSession, String>{
 
+	RunSession findOneByExecutionIdAndRunning(String executionId, Boolean running);
 }
