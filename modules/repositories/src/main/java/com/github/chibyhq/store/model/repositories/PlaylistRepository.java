5c6c5bdd17e37621b308cdb4b85414ee54ef9d68
bcopy <brice.copy@gmail.com>
2019-03-02 14:14:35 +0100

    upgrade to Spring 5 and Java 10

--- modules/repositories/src/main/java/com/github/chibyhq/store/model/repositories/PlaylistRepository.java
+++ modules/repositories/src/main/java/com/github/chibyhq/store/model/repositories/PlaylistRepository.java
@@ -3,7 +3,7 @@ package com.github.chibyhq.store.model.repositories;
 
 import java.util.List;
 
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -11,7 +11,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import com.github.chibyhq.playar.model.Playlist;
 
 @RepositoryRestResource()
-public interface PlaylistRepository extends PagingAndSortingRepository<Playlist, String>, QueryDslPredicateExecutor<Playlist>, PlaylistRepositoryCustom
+public interface PlaylistRepository extends PagingAndSortingRepository<Playlist, String>, QuerydslPredicateExecutor<Playlist>, PlaylistRepositoryCustom
 {
 	    List<Playlist> findByName(@Param("name") String name);
 }
